9434eecac97249473d189e297b6dde83c512faed
Parth <parthvitnor@gmail.com>
2026-03-23 10:29:56 +0530

    feat: add CloudFront distribution with S3 origin, OAC, and HTTPS redirect

--- main.tf
+++ main.tf
@@ -19,3 +19,32 @@ resource "aws_cloudfront_origin_access_control" "OAC" {
     signing_protocol = "sigv4"
 }
 
+resource "aws_cloudfront_distribution" "website_distribution" {
+    origin {
+        domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+        origin_id = "S3-${aws_s3_bucket.website_bucket.id}"
+        origin_access_control_id = aws_cloudfront_origin_access_control.OAC.id
+    }
+
+    enabled = true
+    is_ipv6_enabled = true
+
+    default_cache_behavior {
+        target_origin_id = "S3-${aws_s3_bucket.website_bucket.id}"
+        viewer_protocol_policy = "redirect-to-https"
+        allowed_methods = ["GET", "HEAD"]
+        cached_methods = ["GET", "HEAD"]
+    }
+
+    default_root_object = "index.html"
+
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+    
+  restrictions {
+    geo_restriction {
+      restriction_type = "None"
+    }
+  }
+}
