ee077cf9efc0b84df87173914f72f69a9a0ccf1a
Parth <parthvitnor@gmail.com>
2026-03-23 07:48:17 +0530

    add CloudFront Origin Access Control (OAC) for secure S3 access

--- main.tf
+++ main.tf
@@ -10,3 +10,12 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
     ignore_public_acls = true
     restrict_public_buckets = true
 }
+
+resource "aws_cloudfront_origin_access_control" "OAC" {
+    name = var.cloudfront-oac-name
+    description = var.cloudfront-oac-description
+    origin_access_control_origin_type = "s3"     
+    signing_behavior = "always"
+    signing_protocol = "sigv4"
+}
+
--- variable.tf
+++ variable.tf
@@ -6,3 +6,14 @@ variable "region" {
 variable "bucket_name" {
    default = "static-website-hosting-bucket"
 }
+
+
+variable "cloudfront-oac-name" {
+    default = "OAC-static-web-hosting"
+  
+}
+
+
+variable "cloudfront-oac-description" {
+    default = "OAC for S3 bucket"
+}
