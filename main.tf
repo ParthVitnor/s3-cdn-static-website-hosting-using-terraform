f168f4adddab47306aff5d7e044ee3846a17e531
Parth <parthvitnor@gmail.com>
2026-03-23 12:17:14 +0530

    feat: add S3 bucket policy for CloudFront access

--- main.tf
+++ main.tf
@@ -48,3 +48,34 @@ resource "aws_cloudfront_distribution" "website_distribution" {
     }
   }
 }
+
+resource "aws_s3_bucket_policy" "allow_cloudfront_access" {
+
+    bucket = aws_s3_bucket.website_bucket.id
+
+    policy = jsonencode(
+        {
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowCloudFrontReadAccess",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "cloudfront.amazonaws.com"
+      },
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": "${aws_s3_bucket.website_bucket.arn}/*",
+      "Condition": {
+        "StringEquals": {
+          "AWS:SourceArn": aws_cloudfront_distribution.website_distribution.arn
+        }
+      }
+    }
+  ]
+}
+    )
+  
+}
+
